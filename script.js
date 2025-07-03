2cb23c2679f218ae96cb00eab1740dfe8216fdfc
Tanmay T. <tanmayyy004@gmail.com>
2025-07-03 23:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.querySelectorAll('.card').forEach(card => {
     const rect = card.getBoundingClientRect();
     const x = (e.clientX - rect.left) / rect.width - 0.5;
     const y = (e.clientY - rect.top) / rect.height - 0.5;
-    card.style.transform = `rotateX(${-y * 15}deg) rotateY(${x * 15}deg)`;
+    card.style.transform = `rotateX(${ -y * 25 }deg) rotateY(${ x * 25 }deg)`; // ← Increased angle
   });
 
   card.addEventListener('mouseleave', () => {
