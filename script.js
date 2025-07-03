dd8a4419e83072038360a299dc78d0319eb78cd6
Tanmay T. <tanmayyy004@gmail.com>
2025-07-03 22:47:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const langBtn = document.getElementById('languageToggle');
 
 themeBtn.onclick = () => {
   document.body.classList.toggle('dark');
-  themeBtn.textContent = document.body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
+  themeBtn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 };
 
 langBtn.onclick = () => {
   const isHindi = langBtn.textContent === 'हिन्दी';
   langBtn.textContent = isHindi ? 'English' : 'हिन्दी';
-
   document.querySelectorAll('[data-en]').forEach(el => {
     el.textContent = isHindi ? el.getAttribute('data-hi') : el.getAttribute('data-en');
   });
